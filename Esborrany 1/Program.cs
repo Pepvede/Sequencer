316e380dbb4dd07ef2d2266a0278930bf8fae2ec
Pepv <pepv@hotmail.es>
2017-11-21 19:26:24 +0100

    Visualitzador de partitura

--- Esborrany 1/Program.cs
+++ Esborrany 1/Program.cs
@@ -42,22 +42,179 @@ namespace Esborrany_1
                     }
                 }
             }*/
+
             string fila;
             StreamReader F = new StreamReader("fitxer.txt");
             fila = F.ReadLine();
             F.Close();
             string[] beat = fila.Split('.');
-            int i = 0, j=0, bpm=300, Octava=4, Nota=0;
+            int i = 0, bpm=300, Octava=4, Nota=0, I=0;
             double Frecuencia = 0;
-            while (i<beat.Length)
-            {  
-                Nota = Convert.ToInt32(beat[i]);
+
+            while (I<beat.Length)
+            {
+                Console.Clear();
+
+                Nota = Convert.ToInt32(beat[I]);
                 Frecuencia = ObtenerFrecuencia(Nota,Octava);
-                if(Nota!=0)
+
+                Console.Write("   ");
+                while (i < beat.Length)
+                {
+                    Console.Write("-");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("Si ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 12)&&(beat[i]=="12"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write(" ");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("La ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 11) && (beat[i] == "11") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('#');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if ((Nota == 10) && (beat[i] == "10") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write("-");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("Sl ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 9) && (beat[i] == "9") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('#');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if ((Nota == 8) && (beat[i] == "8") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write(" ");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("Fa ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 7) && (beat[i] == "7") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('#');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if ((Nota == 6) && (beat[i] == "6") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write("-");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("Mi ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 5) && (beat[i] == "5") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write(" ");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("Re ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 4) && (beat[i] == "4") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('#');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if ((Nota == 3) && (beat[i] == "3") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write("-");
+                    i++;
+                }
+                Console.Write("\n");
+                i = 0;
+
+                Console.Write("Do ");
+                while (i < beat.Length)
+                {
+                    if ((Nota == 2) && (beat[i] == "2")&&(I==i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('#');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if ((Nota == 1) && (beat[i] == "1") && (I == i))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('O');
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                        Console.Write(" ");
+                    i++;
+                }
+                Console.Write("\n");
+
+                i = 0;
+
+                if (Nota!=0)
                     Console.Beep((int)Frecuencia, bpm);
                 else
                     Thread.Sleep(bpm);
-                i++;
+                I++;
             }
         }
     }
