555f02046d4a0d065acc12174eaa313c5359ea38
Pepv <pepv@hotmail.es>
2017-11-21 16:48:22 +0100

    Retoquesillos

--- Esborrany 1/Program.cs
+++ Esborrany 1/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.IO;
 
 namespace Esborrany_1
@@ -11,27 +11,52 @@ namespace Esborrany_1
     {
         static double ObtenerFrecuencia(int nota, int octava)
         {
-            double ayudante=(octava-4)*12+(nota-10);
-            ayudante=Math.Pow(2,ayudante/12);
-            return(440.0*ayudante);
+            double auxiliar=(octava-4)*12+(nota-10);
+            auxiliar=Math.Pow(2, auxiliar / 12);
+            return(440.0* auxiliar);
         }
         static void Main(string[] args)
         {
+           /* int i = 0, j = 0;
+            int[,] V = new int[12,8];
+            StreamReader F = new StreamReader("fitxer.txt");
+            string Aux_Partitura;
+            while (i < 12)
+            {
+                Aux_Partitura = F.ReadLine();
+                while (j<8) {
+                    V[i, j] =;
+                    j++;
+                }
+                j = 0;
+                i++;
+            }
+            Console.ReadKey();
+            /*while (j<8)
+            {
+                while (i<12)
+                {
+                    if (V[i,j])
+                    {
+
+                    }
+                }
+            }*/
             string fila;
             StreamReader F = new StreamReader("fitxer.txt");
             fila = F.ReadLine();
             F.Close();
             string[] beat = fila.Split('.');
-            int i = 0, bpm=429, Octava=4, Nota=0;
+            int i = 0, j=0, bpm=300, Octava=4, Nota=0;
             double Frecuencia = 0;
             while (i<beat.Length)
-            {
+            {  
                 Nota = Convert.ToInt32(beat[i]);
                 Frecuencia = ObtenerFrecuencia(Nota,Octava);
                 if(Nota!=0)
                     Console.Beep((int)Frecuencia, bpm);
                 else
-                    System.Threading.Thread.Sleep(bpm);
+                    Thread.Sleep(bpm);
                 i++;
             }
         }
